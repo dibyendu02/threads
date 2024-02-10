37992c1eb49870dcb298b9ebe21ff0a2602fa48d
Dibyendu Das <get2dibyendu@gmail.com>
2024-02-10 22:56:42 +0530

    error resolved

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -9,7 +9,7 @@ async function Page() {
   if (!user) return null; // to avoid typescript warnings
 
 
-  const userInfo = fetchUser(user.id);
+  const userInfo = await fetchUser(user.id);
   // if (userInfo?.onboarded) redirect("/");
 
   const userData = {
