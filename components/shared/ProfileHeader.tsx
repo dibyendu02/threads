28e73fb496643dbfd29583e2ff56001bcd4e3e85
Dibyendu Das <get2dibyendu@gmail.com>
2024-02-16 22:20:58 +0530

    clerk removed

---
+++ components/shared/ProfileHeader.tsx
@@ -0,0 +1,25 @@
+
+interface ProfileHeaderProps {
+    accountId : string,
+    authUserId : string,
+    name : string,
+    username : string,
+    imageUrl : string,
+    bio : string,
+}
+
+const ProfileHeader = ({
+    accountId,
+    authUserId,
+    name,
+    username,
+    imageUrl,
+    bio} : ProfileHeaderProps) => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default ProfileHeader
